20439637751b22333621b5b16919a48c0cf80d98
Oliver Breitwieser <oliver.breitwieser@freiheit.com>
2023-12-10 20:21:01 +0100

    day04: setup

---
+++ day04/src/Lib.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Lib
+  ( part0,
+    part1,
+    part2,
+  )
+where
+
+import Control.Applicative
+import Data.Attoparsec.Text
+import Data.Char
+import Data.Foldable
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import Prelude
+
+part0 :: IO ()
+part0 = return ()
+
+part1 :: IO ()
+part1 = return ()
+
+part2 :: IO ()
+part2 = return ()
+
