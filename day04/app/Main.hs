7854cbdfad11997b7dd301f2b555efa68611e917
Oliver Breitwieser <oliver.breitwieser@freiheit.com>
2023-12-10 21:34:19 +0100

    day04: part 2

--- day04/app/Main.hs
+++ day04/app/Main.hs
@@ -3,4 +3,4 @@ module Main (main) where
 import Lib
 
 main :: IO ()
-main = part0 >> part1
+main = part0 >> part1 >> part2
--- day04/src/Lib.hs
+++ day04/src/Lib.hs
@@ -25,6 +25,8 @@ part0 = do
   s <- TIO.readFile "debug.txt"
   putStr "Part 1 debug (expecting 13): "
   either error part1inner $ parseOnly parseCards s
+  putStr "Part 2 debug (expecting 30): "
+  either error part2inner $ parseOnly parseCards s
 
 part1 :: IO ()
 part1 = do
@@ -33,11 +35,19 @@ part1 = do
   either error part1inner $ parseOnly parseCards s
 
 part2 :: IO ()
-part2 = return ()
+part2 = do
+  s <- TIO.readFile "input.txt"
+  putStr "Part 2: "
+  either error part2inner $ parseOnly parseCards s
 
 part1inner :: [Card] -> IO ()
 part1inner cards = do
   print . sum . map points $ cards
+
+part2inner :: [Card] -> IO ()
+part2inner cards = do
+  print . sum . countWinningCards $ cards
+
 
 data Card = Card
   { cardId :: Int,
@@ -47,11 +57,26 @@ data Card = Card
   deriving (Show)
 
 points :: Card -> Int
-points Card {..} = go . length $ winning `intersect` numbers
+points = go . numMatches
   where
    go 0 = 0
    go n = shiftL 1 (n-1)
 
+numMatches :: Card -> Int
+numMatches Card {..} = length $ winning `intersect` numbers
+
+countWinningCards :: [Card] -> [Int]
+countWinningCards = go [] (repeat 1)
+  where
+    go :: [Int] -> [Int] -> [Card] -> [Int]
+    go total _ [] = total
+    go total (co:copies) (ca:cards) = go (co:total) (incFirstNumBy (numMatches ca) co copies) cards
+    go _ [] _ = undefined
+
+incFirstNumBy :: Int -> Int -> [Int] -> [Int]
+incFirstNumBy 0 _ remaining = remaining
+incFirstNumBy n by (r:remaining) = (r+by):incFirstNumBy (n-1) by remaining
+incFirstNumBy _ _ [] = undefined
 
 ensureParsed :: Result a -> Result a
 ensureParsed (Partial p) = ensureParsed $ feed (Partial p) ""
