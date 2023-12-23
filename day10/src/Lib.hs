eeec58015ad6278ab96d2cec5c9a6e17ef492a17
Oliver Breitwieser <oliver.breitwieser@freiheit.com>
2023-12-23 01:44:04 +0100

    day10: part 2

--- day10/src/Lib.hs
+++ day10/src/Lib.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE DuplicateRecordFields #-}{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
@@ -16,9 +17,10 @@ import Data.Char
 import Data.Foldable
 import Data.Functor
 import Data.List (intersect, sortBy)
-import qualified Data.Map.Strict as M
 import Data.Map.Strict ((!))
+import qualified Data.Map.Strict as M
 import Data.Maybe
+import qualified Data.Set as S
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import Data.Text.Internal.Fusion.Common (findBy)
@@ -34,8 +36,18 @@ part0 = do
   putStr "Part 1 debug complex (expecting 8): "
   s2 <- TIO.readFile "debug2.txt"
   either error part1inner $ parseOnly parseInput1 s2
-  putStr "Part 2 debug (expecting 71503): "
-  either error part2inner $ parseOnly parseInput2 s1
+  s3 <- TIO.readFile "debug3.txt"
+  putStr "Part 2 debug (expecting 4): "
+  either error part2inner $ parseOnly parseInput2 s3
+  s4 <- TIO.readFile "debug4.txt"
+  putStr "Part 2 debug (expecting 8): "
+  either error part2inner $ parseOnly parseInput2 s4
+  s5 <- TIO.readFile "debug5.txt"
+  putStr "Part 2 debug (expecting 4): "
+  either error part2inner $ parseOnly parseInput2 s5
+  s6 <- TIO.readFile "debug6.txt"
+  putStr "Part 2 debug (expecting 10): "
+  either error part2inner $ parseOnly parseInput2 s6
 
 -- putStr "Part 2 debug (expecting 46): "
 -- either error part2inner $ parseOnly parseInput2 s
@@ -54,7 +66,7 @@ part2 = do
 
 part1inner :: Input1 -> IO ()
 part1inner input = do
-  let m= toMap input
+  let m = toMap input
   -- print m
   let start = getStart input
   let pipeStart = determinePipe m start
@@ -67,19 +79,30 @@ part1inner input = do
 
 part2inner :: Input2 -> IO ()
 part2inner input = do
-  print input
+  let lenSideY = length input
+  let lenSideX = length . head $ input
+  let lenSide = (lenSideX, lenSideY)
+  let mRaw = toMap input
+  let start = getStart input
+  let pipeStart = determinePipe mRaw start
+  let m = M.insert start pipeStart mRaw
+  let distances = computeDistances m start
+  let loop = S.fromList . M.keys $ distances
+  let allSets = fillAllSets lenSide loop
+  -- print allSets
+  print . sum $ map length $ filter (isInner (filterLoop loop m)) allSets
 
 -- print . getPossibilities $ input
 
 type Input1 = [[Tile]]
 
-type Input2 = ()
+type Input2 = Input1
 
 parseInput1 :: Parser Input1
 parseInput1 = parseTiles
 
 parseInput2 :: Parser Input2
-parseInput2 = return ()
+parseInput2 = parseTiles
 
 -- | is a vertical pipe connecting north and south.
 -- - is a horizontal pipe connecting east and west.
@@ -190,29 +213,102 @@ computeDistances pipeMap start = bfs (M.singleton start 0) [start]
   where
     bfs :: M.Map (Int, Int) Int -> [(Int, Int)] -> M.Map (Int, Int) Int
     bfs distances [] = distances
-    bfs distances (current:queue) = let
-        nn = neighbors current (pipeMap ! current)
+    bfs distances (current : queue) =
+      let nn = neighbors current (pipeMap ! current)
 
-        foldedUpdate :: M.Map (Int, Int) Int -> (Int, Int) -> M.Map (Int, Int) Int
-        foldedUpdate = flip $ M.alter $ update (distances ! current)
+          foldedUpdate :: M.Map (Int, Int) Int -> (Int, Int) -> M.Map (Int, Int) Int
+          foldedUpdate = flip $ M.alter $ update (distances ! current)
 
-        updatedDistances :: M.Map (Int, Int) Int
-        updatedDistances = foldl' foldedUpdate distances nn
+          updatedDistances :: M.Map (Int, Int) Int
+          updatedDistances = foldl' foldedUpdate distances nn
 
-        appendedQueue = (queue ++ filter (`M.notMember` distances) nn)
-      in
-        bfs updatedDistances appendedQueue
+          appendedQueue = (queue ++ filter (`M.notMember` distances) nn)
+       in bfs updatedDistances appendedQueue
 
     update :: Int -> Maybe Int -> Maybe Int
-    update currentDistance Nothing = Just $ currentDistance+1
+    update currentDistance Nothing = Just $ currentDistance + 1
     update _ (Just d) = Just d
 
+fillAllSets :: (Int, Int) -> S.Set (Int, Int) -> [S.Set (Int, Int)]
+fillAllSets lenSide loop = go initial []
+  where
+    initial :: S.Set (Int, Int)
+    initial = S.fromList [(i, j) | i <- [0 .. fst lenSide - 1], j <- [0 .. snd lenSide - 1]] `S.difference` loop
+
+    go :: S.Set (Int, Int) -> [S.Set (Int, Int)] -> [S.Set (Int, Int)]
+    go remaining returnValue
+      | S.null remaining = returnValue
+      | otherwise =
+          let current = head $ S.toList remaining
+              fill = fillSet lenSide loop current
+           in go (remaining `S.difference` fill) (fill : returnValue)
+
+fillSet :: (Int, Int) -> S.Set (Int, Int) -> (Int, Int) -> S.Set (Int, Int)
+fillSet lenSide bound start = bfs S.empty [start]
+  where
+    inBound :: (Int, Int) -> Bool
+    inBound (x, y) = 0 <= x && 0 <= y && x < fst lenSide && y < snd lenSide
+
+    bfs :: S.Set (Int, Int) -> [(Int, Int)] -> S.Set (Int, Int)
+    bfs fill [] = fill
+    bfs fill (current : queue)
+      | S.member current fill = bfs fill queue
+      | otherwise =
+          let allNN = allNeighbors current
+              filteredNN = filter (`S.notMember` bound) $ filter (`S.notMember` fill) $ filter inBound allNN
+              appendedQueue = filteredNN ++ queue
+           in bfs (S.insert current fill) appendedQueue
 
 neighbors :: (Int, Int) -> Pipe -> [(Int, Int)]
-neighbors (x, y) (Pipe NorthSouth Vertical) = [(x, y-1), (x, y+1)]
-neighbors (x, y) (Pipe Horizontal WestEast) = [(x-1, y), (x+1, y)]
-neighbors (x, y) (Pipe North East) = [(x, y-1), (x+1, y)]
-neighbors (x, y) (Pipe North West) = [(x, y-1), (x-1, y)]
-neighbors (x, y) (Pipe South West) = [(x,y+1), (x-1, y)]
-neighbors (x, y) (Pipe South East) = [(x,y+1), (x+1, y)]
+neighbors (x, y) (Pipe NorthSouth Vertical) = [(x, y - 1), (x, y + 1)]
+neighbors (x, y) (Pipe Horizontal WestEast) = [(x - 1, y), (x + 1, y)]
+neighbors (x, y) (Pipe North East) = [(x, y - 1), (x + 1, y)]
+neighbors (x, y) (Pipe North West) = [(x, y - 1), (x - 1, y)]
+neighbors (x, y) (Pipe South West) = [(x, y + 1), (x - 1, y)]
+neighbors (x, y) (Pipe South East) = [(x, y + 1), (x + 1, y)]
 neighbors _ p = error $ "invalid pipe: " ++ show p
+
+allNeighbors :: (Int, Int) -> [(Int, Int)]
+allNeighbors (x, y) =
+  [ (x + 1, y),
+    (x - 1, y),
+    (x, y + 1),
+    (x, y - 1)
+  ]
+
+-- only include pipes present in loop
+filterLoop :: S.Set (Int, Int) -> PipeMap -> PipeMap
+filterLoop loop = M.filterWithKey (\k _ -> S.member k loop)
+
+isInner :: PipeMap -> S.Set (Int, Int) -> Bool
+isInner m = isInner' m . head . S.toList
+
+isInner' :: PipeMap -> (Int, Int) -> Bool
+isInner' m p = numCrossings `mod` 2 == 1
+  where
+    numCrossings = go 0 Nothing p
+
+    -- use half crossing to differentiate between
+    -- ─┐    ┌─  &  ──┐ ┌──
+    --  └─  ─┘   &  ──┘ └──
+    go :: Int -> Maybe HalfCrossing -> (Int, Int) -> Int
+    go current _ (_, -1) = current
+    go current hc c@(x, y) = go (current+update) nextHC next
+       where
+          next = (x, y - 1)
+          pipe = M.lookup c m
+
+          update = case (pipe, hc) of
+            (Just (Pipe Horizontal WestEast), _) -> 1
+            (Just (Pipe South West), Just HCFromEast) -> 1
+            (Just (Pipe South East), Just HCFromWest) -> 1
+            _ -> 0
+
+          nextHC = case (hc, pipe) of
+            (_, Just (Pipe North West)) -> Just HCFromWest
+            (_, Just (Pipe North East)) -> Just HCFromEast
+            (_, Just (Pipe South West)) -> Nothing
+            (_, Just (Pipe South East)) -> Nothing
+            (curHC, _) -> curHC
+
+data HalfCrossing = HCFromWest | HCFromEast deriving (Show)
