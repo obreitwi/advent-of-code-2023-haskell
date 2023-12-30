302f6a074e4ad2cdbc84e6ebdebc59f8389003a8
Oliver Breitwieser <oliver.breitwieser@freiheit.com>
2023-12-30 18:02:56 +0100

    day12: clean-up

--- day12/src/Lib.hs
+++ day12/src/Lib.hs
@@ -36,9 +36,6 @@ part0 = do
   putStr "Part 2 debug (expecting 525152): "
   either error part2inner $ parseOnly parseInput2 s
 
--- putStr "Part 2 debug (expecting 46): "
--- either error part2inner $ parseOnly parseInput2 s
-
 part1 :: IO ()
 part1 = do
   s <- TIO.readFile "input.txt"
@@ -59,19 +56,7 @@ part1inner input = do
 
 part2inner :: Input2 -> IO ()
 part2inner input = do
-  -- let damaged = map (splitDamaged . getStates) input
-  -- let damagedConcat = S.fromList . concat $ damaged
-  -- let target = map getDamaged input
-
-  -- mapM_ print damagedConcat
-  -- mapM_ (print . \x -> (x, possibleCombinations x)) damagedConcat
-  -- mapM_ print $ head damaged
-  -- print $ head target
-  -- putStrLn ""
-  -- mapM_ (mapM_ print . M.toList . flip filterPossible (head target)) $ head damaged
-  -- mapM_ print $ zipWith countValidDamaged target damaged
-  -- print . sum $ zipWith countValidDamaged target damaged
-  mapM_ (print . countPossibilitiesAllAtOnce) input
+  -- mapM_ (print . countPossibilitiesAllAtOnce) input
   print . sum . map countPossibilitiesAllAtOnce $ input
 
 type Input1 = [SpringConfig]
@@ -85,12 +70,6 @@ parseInput2 :: Parser Input2
 parseInput2 = do map unfold <$> parseInput1
 
 data SpringConfig = SC [SpringState] [Int] deriving (Show, Eq)
-
-getStates :: SpringConfig -> [SpringState]
-getStates (SC states _) = states
-
-getDamaged :: SpringConfig -> [Int]
-getDamaged (SC _ dmgd) = dmgd
 
 data SpringState = Operational | Damaged | Unknown deriving (Show, Eq, Ord, Enum)
 
@@ -107,113 +86,11 @@ parseSpringState = char '.' $> Operational <|> char '#' $> Damaged <|> char '?' 
 unfold :: SpringConfig -> SpringConfig
 unfold (SC cfgs dmgd) = SC (intercalate [Unknown] (replicate 5 cfgs)) (concat $ replicate 5 dmgd)
 
--- split springs into sections seperated by at least one working spring
-splitDamaged :: [SpringState] -> [[SpringState]]
-splitDamaged = go [] []
-  where
-    --    accumulated        current split    input
-    go :: [[SpringState]] -> [SpringState] -> [SpringState] -> [[SpringState]]
-    go accum [] [] = accum
-    go accum current [] = go (accum ++ [current]) [] []
-    go accum [] (Operational : next) = go accum [] next
-    go accum current (Operational : next) = go (accum ++ [current]) [] next
-    go accum current (top : next) = go accum (current ++ [top]) next
-
--- NOTE: expects spring state to not contain Operational
-possibleCombinations :: [SpringState] -> M.Map [Int] Int
-possibleCombinations = foldl' (flip $ M.alter p1) M.empty . go 0
-  where
-    -- go :: M.Map [Int] Int -> [Int] -> [SpringState] -> M.Map [Int] Int
-    -- go accum (current:other) (Damaged:ss) = go accum (current+1:other) ss
-    -- go accum (current:other) (Unknown:ss) = M.alter p1
-    -- accum (current+1:other) ss
-
-    go :: Int -> [SpringState] -> [[Int]]
-    go 0 [] = [[]]
-    go d [] = [[d]]
-    go d (Damaged : ss) = traceShow (length ss) $ go (d + 1) ss
-    go 0 (Unknown : ss) = traceShow (length ss) $ go 0 ss ++ go 1 ss
-    go d (Unknown : ss) = traceShow (length ss) $ go (d + 1) ss ++ map ([d] ++) (go 0 ss)
-    go _ (Operational : _) = error "Operational not expected"
-
-    p1 :: Maybe Int -> Maybe Int
-    p1 Nothing = Just 1
-    p1 (Just x) = Just $ x + 1
-
-countValidDamaged :: [Int] -> [[SpringState]] -> Int
-countValidDamaged ttt = fromMaybe 0 . flip (M.!?) [] . foldl folder initial
-  where
-    initial :: M.Map [Int] Int
-    initial = M.singleton ttt 1
-
-    folder :: M.Map [Int] Int -> [SpringState] -> M.Map [Int] Int
-    folder x y = traceShow (x, y) $ filterDamagedGroup y x
-
--- folder x y = filterDamagedGroup y x
-
--- filter all out all possible combinations that cannot be reached for a "damanged group" (i.e. a group without Operational states)
-filterDamagedGroup :: [SpringState] -> M.Map [Int] Int -> M.Map [Int] Int
--- filterDamagedGroup s = traceShowId . foldl' (M.unionWith (+)) M.empty . map (\(k, v) -> M.map (v +) (countPossibilities s k)) . M.toList
-filterDamagedGroup s = foldl' (M.unionWith (+)) M.empty . map (\(k, v) -> M.map (v *) (countPossibilities s k)) . M.toList
-
--- NOTE: expects spring state to not contain Operational
-countPossibilities :: [SpringState] -> [Int] -> M.Map [Int] Int
-countPossibilities sss ttt = foldl' (flip $ M.alter p1) M.empty $ mo 0 sss ttt
-  where
-    mo :: Int -> [SpringState] -> [Int] -> [[Int]]
-    mo = Memo.memo3 Memo.integral (Memo.list Memo.enum) (Memo.list Memo.integral) go'
-
-    -- go :: M.Map [Int] Int -> [Int] -> [SpringState] -> M.Map [Int] Int
-    -- go accum (current:other) (Damaged:ss) = go accum (current+1:other) ss
-    -- go accum (current:other) (Unknown:ss) = M.alter p1
-    -- accum (current+1:other) ss
-
-    go' :: Int -> [SpringState] -> [Int] -> [[Int]]
-    go' d ss tt = trace (printf "[Depth] %d [SpringStates] %s [Targets] %s" d (show ss) (show tt)) $ go d ss tt
-
-    -- current damaged, input, target groups
-    go :: Int -> [SpringState] -> [Int] -> [[Int]]
-    go 0 [] [] = [[]]
-    go _ [] [] = []
-    -- go 0 [] targets = [targets]
-    go 0 [] targets = []
-    go d [] (t : tt)
-      | d == t = [tt]
-      | otherwise = []
-    go _ (Damaged : ss) [] = []
-    go d (Damaged : ss) allTargets@(t : _)
-      | d >= t = [] -- more damaged springs than the current contiguous region allows
-      | otherwise = mo (d + 1) ss allTargets
-    go d (Unknown : ss) [] = mo d ss []
-    go 0 (Unknown : ss) targets =
-      let choseDamaged = mo 1 ss targets
-          choseOperational = mo 0 ss targets
-          remainingFixedDamaged = length . filter (== Damaged) $ ss
-          totalDamagedExpected = sum targets
-       in -- in choseOperational `seq` choseDamaged `seq` choseDamaged ++ choseOperational
-          if remainingFixedDamaged < totalDamagedExpected -- only choose both if we can expect to succeed
-            then choseDamaged ++ choseOperational
-            else choseOperational
-    go d (Unknown : ss) allTargets@(t : targets)
-      | d == t = mo 0 ss targets
-      | d < t = mo (d + 1) ss allTargets
-      | otherwise = []
-    go _ (Operational : _) _ = error "encountered fixed Operational spring"
-
-    p1 :: Maybe Int -> Maybe Int
-    p1 Nothing = Just 1
-    p1 (Just x) = Just $ x + 1
-
 countPossibilitiesAllAtOnce :: SpringConfig -> Int
 countPossibilitiesAllAtOnce (SC sss ttt) = mo 0 sss ttt
   where
     mo :: Int -> [SpringState] -> [Int] -> Int
     mo = Memo.memo3 Memo.integral (Memo.list Memo.enum) (Memo.list Memo.integral) go'
-
-    -- go :: M.Map [Int] Int -> [Int] -> [SpringState] -> M.Map [Int] Int
-    -- go accum (current:other) (Damaged:ss) = go accum (current+1:other) ss
-    -- go accum (current:other) (Unknown:ss) = M.alter p1
-    -- accum (current+1:other) ss
 
     go' :: Int -> [SpringState] -> [Int] -> Int
     -- go' d ss tt = trace (printf "[Depth] %d [SpringStates] %s [Targets] %s" d (show ss) (show tt)) $ go d ss tt
@@ -232,13 +109,7 @@ countPossibilitiesAllAtOnce (SC sss ttt) = mo 0 sss ttt
     go 0 (Unknown : ss) targets =
       let choseDamaged = mo 1 ss targets
           choseOperational = mo 0 ss targets
-          -- remainingFixedDamaged = length . filter (== Damaged) $ ss
-          -- totalDamagedExpected = sum targets
-       -- in choseOperational `seq` choseDamaged `seq` choseDamaged + choseOperational
        in choseDamaged + choseOperational
-          -- if remainingFixedDamaged < totalDamagedExpected -- only choose both if we can expect to succeed
-            -- then choseDamaged + choseOperational
-            -- else choseOperational
     go d (Unknown : ss) allTargets@(t : targets)
       | d == t = mo 0 ss targets
       | d < t = mo (d + 1) ss allTargets
@@ -247,7 +118,7 @@ countPossibilitiesAllAtOnce (SC sss ttt) = mo 0 sss ttt
     go d (Operational : ss) (t : targets)
       | d == t = go 0 ss targets
       | otherwise = 0
-    go d (Operational : ss) [] = 0
+    go _ (Operational : _) [] = 0
 
 countValid :: SpringConfig -> Int
 countValid (SC configs damaged) = go Operational configs damaged
