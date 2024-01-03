8263e6cd5303ba7944f5f28bafae2118f70adb49
Oliver Breitwieser <oliver.breitwieser@freiheit.com>
2024-01-03 18:32:32 +0100

    day 14: part 2

--- day14/src/Lib.hs
+++ day14/src/Lib.hs
@@ -11,26 +11,12 @@ module Lib
   )
 where
 
-import Data.Attoparsec.Text hiding (takeWhile, take)
+import Data.Attoparsec.Text hiding (take, takeWhile)
+import qualified Data.Map.Strict as M
+import qualified Data.MemoCombinators as Memo
 import qualified Data.Text.IO as TIO
 import Rebase.Prelude
 import Prelude ()
-
--- import Control.Applicative
--- import Data.Bits
--- import Data.Char
--- import Data.Foldable
--- import Data.Functor
--- import Data.List (intersect, sortBy)
--- import Data.Text.Internal.Fusion.Common (findBy)
--- import Debug.Trace
--- import Prelude hiding (takeWhile)
--- import qualified Data.Bifunctor as B
--- import qualified Data.List as L
--- import qualified Data.Map.Strict as M
--- import qualified Data.Text as T
--- import qualified Data.Vector as V
--- import qualified Data.Vector.Unboxed as U
 
 part0 :: IO ()
 part0 = do
@@ -54,33 +40,72 @@ part2 = do
 
 part1inner :: Input1 -> IO ()
 part1inner input = do
-  -- print input
-  -- print $ dropColumn [Empty, Cube, Empty, Round]
-  -- putStrLn ""
-  -- mapM_ print . rotateCounterClock $ input
-  -- putStrLn ""
-  -- mapM_ (print . dropColumn) . rotateCounterClock $ input
-  -- putStrLn ""
   print . calculateLoadWest . map dropColumn . rotateCounterClock $ input
 
 part2inner :: Input2 -> IO ()
 part2inner input = do
-  print input
+  when (input /= (rotateCounterClock . rotateCounterClock . rotateCounterClock . rotateCounterClock) input) $
+    error "rotation not invariant"
+
+  print . calculateLoadWest . rotateCounterClock $ rotateAndDropN 1000000000 input
 
 type Input1 = [[Cell]]
 
-type Input2 = ()
+type Input2 = Input1
 
 parseInput1 :: Parser Input1
 parseInput1 = sepBy1' (many1' parseCell) endOfLine
 
 parseInput2 :: Parser Input2
-parseInput2 = return ()
+parseInput2 = parseInput1
 
-data Cell = Empty | Round | Cube deriving (Show, Eq)
+data Cell = Empty | Round | Cube deriving (Show, Eq, Enum, Ord)
 
 parseCell :: Parser Cell
 parseCell = char '.' $> Empty <|> char 'O' $> Round <|> char '#' $> Cube
+
+rotateAndDropN :: Int -> [[Cell]] -> [[Cell]]
+rotateAndDropN = go' M.empty
+  where
+    mo :: [[Cell]] -> [[Cell]]
+    mo = Memo.list (Memo.list Memo.enum) fullRotationDrop
+
+    go :: Int -> [[Cell]] -> [[Cell]]
+    go 0 cc = cc
+    go n prev =
+      let next = mo prev
+       in if prev == next
+            then next
+            else trace (printf "[%d] %d" n (calculateLoadWest . rotateCounterClock $ next)) $ go (n - 1) next
+
+    go' :: M.Map [[Cell]] Int -> Int -> [[Cell]] -> [[Cell]]
+    go' _ 0 prev = prev
+    go' seen n prev =
+      let next = fullRotationDrop prev
+       in case seen M.!? next of
+            Nothing -> go' (M.insert prev n seen) (n - 1) next
+            Just seenAt -> let n' = (n `rem` (seenAt - n + 1)) in go' M.empty (n'-1) next
+
+fullRotationDrop :: [[Cell]] -> [[Cell]]
+fullRotationDrop =
+  rotateCounterClock
+    . rotateCounterClock
+    . map dropColumn
+    . rotateCounterClock
+    . rotateCounterClock
+    . rotateCounterClock -- east
+    . map dropColumn
+    . rotateClock
+    . map dropColumn -- south
+    . rotateClock -- west
+    . map dropColumn
+    . rotateCounterClock -- north
+
+rotDrop :: [[Cell]] -> [[Cell]]
+rotDrop = map dropColumn . rotateCounterClock
+
+rotateClock :: [[Cell]] -> [[Cell]]
+rotateClock = rotateCounterClock . rotateCounterClock . rotateCounterClock
 
 rotateCounterClock :: [[Cell]] -> [[Cell]]
 rotateCounterClock = reverse . go
@@ -95,9 +120,20 @@ calculateLoadWest :: [[Cell]] -> Int
 calculateLoadWest = sum . map calculateLoadColumn
 
 calculateLoadColumn :: [Cell] -> Int
-calculateLoadColumn = sum . map fst . filter ((== Round) . snd) . zip [1..] . reverse
+calculateLoadColumn = sum . map fst . filter ((== Round) . snd) . zip [1 ..] . reverse
 
 dropColumn :: [Cell] -> [Cell]
 dropColumn [] = []
-dropColumn (Cube:cc) = Cube : dropColumn cc
+dropColumn (Cube : cc) = Cube : dropColumn cc
 dropColumn cc = (uncurry (++) . partition (== Round) . takeWhile (/= Cube) $ cc) ++ dropColumn (dropWhile (/= Cube) cc)
+
+printCells :: [[Cell]] -> IO ()
+printCells cc = mapM_ printCellLine cc >> putChar '\n'
+
+printCellLine :: [Cell] -> IO ()
+printCellLine l = mapM_ printCell l >> putChar '\n'
+  where
+    printCell :: Cell -> IO ()
+    printCell Cube = putChar '#'
+    printCell Empty = putChar '.'
+    printCell Round = putChar 'O'
