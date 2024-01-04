0a1a130ce69fdfd78949671015ebc72c051ee445
Oliver Breitwieser <oliver.breitwieser@freiheit.com>
2024-01-04 01:31:45 +0100

    day 15: part 2

--- day15/src/Lib.hs
+++ day15/src/Lib.hs
@@ -16,16 +16,16 @@ import qualified Data.Map.Strict as M
 import qualified Data.MemoCombinators as Memo
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+import qualified Rebase.Data.UUID as T
 import Rebase.Prelude hiding (takeWhile)
 import Prelude ()
-import qualified Rebase.Data.UUID as T
 
 part0 :: IO ()
 part0 = do
   s <- TIO.readFile "debug.txt"
   putStr "Part 1 debug (expecting 1320): "
   either error part1inner $ parseOnly parseInput1 s
-  putStr "Part 2 debug (expecting XXX): "
+  putStr "Part 2 debug (expecting 145): "
   either error part2inner $ parseOnly parseInput2 s
 
 part1 :: IO ()
@@ -48,21 +48,72 @@ part1inner input = do
 
 part2inner :: Input2 -> IO ()
 part2inner input = do
-  print input
+  -- mapM_ print input
+  -- print . sum . map hashInstruction $ input
+  -- mapM_ print . M.toList . performInstructions $ input
+  print  . lensScore . performInstructions $ input
 
 type Input1 = [Text]
 
-type Input2 = ()
+type Input2 = [Instruction]
 
 parseInput1 :: Parser Input1
 parseInput1 = sepBy1' (takeWhile (`notElem` [',', '\n'])) (char ',')
 
 parseInput2 :: Parser Input2
-parseInput2 = return ()
+parseInput2 = sepBy1' parseInstruction (char ',')
 
 hashSingle :: Text -> Int
 hashSingle = hash' 0 . T.unpack
   where
     hash' :: Int -> String -> Int
     hash' h [] = h
-    hash' h (c:cc) = hash' (((h + fromEnum c) * 17) `rem` 256) cc
+    hash' h (c : cc) = hash' (((h + fromEnum c) * 17) `rem` 256) cc
+
+data Instruction = Inst Text Operation deriving (Show, Eq, Ord)
+
+data Operation = Removal | Replace Int deriving (Show, Eq, Ord)
+
+parseInstruction :: Parser Instruction
+parseInstruction = Inst <$> takeWhile isAlpha <*> parseOperation
+
+parseOperation :: Parser Operation
+parseOperation = char '-' $> Removal <|> char '=' $> Replace <*> decimal
+
+hashInstruction :: Instruction -> Int
+hashInstruction (Inst label _ ) = hashSingle label
+
+textInstruction :: Instruction -> Text
+textInstruction (Inst label operation) = label <> textOperation operation
+
+textOperation :: Operation -> Text
+textOperation Removal = T.singleton '-'
+textOperation (Replace val) = T.singleton '=' <> T.pack (show val)
+
+data Lens = Lens {lensLabel :: Text, lensValue :: Int} deriving (Show, Eq, Ord)
+
+performInstructions :: [Instruction] -> M.Map Int [Lens]
+performInstructions = foldl' (flip perform) M.empty
+  where
+    perform :: Instruction -> M.Map Int [Lens] -> M.Map Int [Lens]
+    perform i = M.alter (applyInstruction i) (hashInstruction i)
+
+applyInstruction :: Instruction -> Maybe [Lens] -> Maybe [Lens]
+applyInstruction (Inst _ Removal) Nothing = Nothing
+applyInstruction (Inst _ Removal) (Just []) = Nothing
+applyInstruction (Inst label Removal) (Just box) = Just $ filter ((/= label) . lensLabel) box
+applyInstruction (Inst lensLabel (Replace lensValue)) Nothing = Just [Lens {..}]
+applyInstruction (Inst lensLabel (Replace lensValue)) (Just []) = Just [Lens {..}]
+applyInstruction (Inst label (Replace value)) (Just box)
+  | isNothing . find ((== label) . lensLabel) $ box = Just $ box ++ [Lens {lensLabel = label, lensValue = value}]
+  | otherwise = Just $ map updateLens box
+  where
+    updateLens lens
+      | lensLabel lens == label = Lens {lensValue = value, lensLabel = label }
+      | otherwise = lens
+
+lensScore :: M.Map Int [Lens] -> Int
+lensScore = sum . map (\(idx, box) -> boxScore (idx+1) box) . M.toList
+
+boxScore :: Int -> [Lens] -> Int
+boxScore boxIdx = sum . zipWith (\lensIdx lens -> boxIdx * lensIdx * lensValue lens) [1..]
